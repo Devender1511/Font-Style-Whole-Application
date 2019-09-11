eba181c255538faf7858c7c4fe4cec7366db3e4e
Devendersaini <saini.devo15@gmail.com>
2019-09-11 16:37:55 +0530

    With Shimmer Layout

--- app/src/main/java/in/vcaretechs/textview/MainActivity.java
+++ app/src/main/java/in/vcaretechs/textview/MainActivity.java
@@ -2,7 +2,10 @@ package in.vcaretechs.textview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_shimmer=findViewById(R.id.btn_shimmer);
+        btn_shimmer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this
+                ,Shimmer.class);
+                startActivity(intent);
+            }
+        });
     }
 }
